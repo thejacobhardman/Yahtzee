476b1b1da5998e20aed925d25833ff7ed9d6284b
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2021-03-05 14:59:32 -0700

    add documentation to new game files

--- src/Yahtzee_Score_Card_Tester.java
+++ src/Yahtzee_Score_Card_Tester.java
@@ -1,3 +1,19 @@
+/*******************************************************************************
+*                      Assignment 6 -- Yahtzee Solo Game                       *
+*                                                                              *
+* PROGRAMMER:        Jacob Hardman hard7293@bears.unco.edu                     *
+* CLASS:             CS200 – Object Oriented Programming                       *
+* INSTRUCTOR:        Dean Zeller                                               *
+* TERM:              Spring 2021                                               *
+* SUBMISSION DATE:   3/5/2021	                                               *
+*                                                                              *
+* DESCRIPTION                                                                  *
+* This file tests the functionality of the Yahtzee_Score_Card.java file.       *
+*                                                                              *
+* COPYRIGHT:                                                                   *
+* This program is copyright (c)2021 Dean Zeller and Jacob Hardman.             *
+*                                                                              *
+*******************************************************************************/
 
 public class Yahtzee_Score_Card_Tester {
 
--- src/Yahtzee_Solo_Game.java
+++ src/Yahtzee_Solo_Game.java
@@ -1,3 +1,23 @@
+/*******************************************************************************
+*                      Assignment 6 -- Yahtzee Solo Game                       *
+*                                                                              *
+* PROGRAMMER:        Jacob Hardman hard7293@bears.unco.edu                     *
+* CLASS:             CS200 – Object Oriented Programming                       *
+* INSTRUCTOR:        Dean Zeller                                               *
+* TERM:              Spring 2021                                               *
+* SUBMISSION DATE:   3/5/2021	                                               *
+*                                                                              *
+* DESCRIPTION                                                                  *
+* This file implements the functionality of a score card used in the game      *
+* of Yahtzee.                                                                  *
+*                                                                              *
+* ATTRIBUTES:                                                                  *
+*         dice: an ArrayList that holds each individual die in the hand.       *
+*                                                                              *
+* COPYRIGHT:                                                                   *
+* This program is copyright (c)2021 Dean Zeller and Jacob Hardman.             *
+*                                                                              *
+*******************************************************************************/
 
 public class Yahtzee_Solo_Game {
 
